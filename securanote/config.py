62dd3a305f47d7a7176bb778875246d599852d6a
Harsha-k04 <harsha.nair04@gmail.com>
2025-07-03 22:47:52 +0530

    Update config.py

--- securanote/config.py
+++ securanote/config.py
@@ -2,5 +2,5 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get("SECRET_KEY", "fallback-secret-key")
-    SQLALCHEMY_DATABASE_URI = "postgresql://postgres:ZABRKNTxFDsMLAWgzQLubWgyzBlGXftl@crossover.proxy.rlwy.net:54959/railway"
+    SQLALCHEMY_DATABASE_URI = os.getenv("SQLALCHEMY_DATABASE_URI")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
