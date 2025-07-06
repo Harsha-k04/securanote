ed941e4397f415b15a1164a8506984b216206e83
Harsha-k04 <harsha.nair04@gmail.com>
2025-07-06 13:22:16 +0530

    Update run.py

--- run.py
+++ run.py
@@ -33,3 +33,8 @@ def serve_chacha_video():
     except Exception as e:
         return f"Error: {e}", 500
 application = app
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True, host="0.0.0.0", port=port)
+
