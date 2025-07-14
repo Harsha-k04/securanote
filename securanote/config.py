5345bea2a098ce3de2d249c69f6031933a885da2
Harsha-k04 <harsha.nair04@gmail.com>
2025-07-14 23:19:23 +0530

    Changed OTP sender email and added adaptive auth tracking

--- securanote/config.py
+++ securanote/config.py
@@ -2,5 +2,5 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get("SECRET_KEY", "fallback-secret-key")
-    SQLALCHEMY_DATABASE_URI = os.getenv("SQLALCHEMY_DATABASE_URI")
+    SQLALCHEMY_DATABASE_URI = "postgresql://postgres:teVVjuepQZrWQFlmjVngZJEcnHAVebmy@postgres.railway.internal:5432/railway"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
