e4db9d57ac8e15d396eedefc658b425ea91bb6d3
Harsha-k04 <harsha.nair04@gmail.com>
2025-08-04 14:06:31 +0530

    Update config.py

--- securanote/config.py
+++ securanote/config.py
@@ -2,5 +2,5 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get("SECRET_KEY", "fallback-secret-key")
-    SQLALCHEMY_DATABASE_URI = "postgresql://securanote_user:OsKWfhyrOmeJueiEw3UmNVUVH9TrqDVG@dpg-d285r8m3jp1c7380kvn0-a/securanote_db"
+    SQLALCHEMY_DATABASE_URI = "postgresql://securanote_user:OsKWfhyrOmeJueiEw3UmNVUVH9TrqDVG@dpg-d285r8m3jp1c7380kvn0-a.oregon-postgres.render.com/securanote_db"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
